2efbae3ea9a00feddaaab75b600dc3ebacf6b727
Martin Vidmar <martin.vidmar@seznam.cz>
2025-10-30 12:32:50 +0100

    master: link styling

--- app/_homePageBlocks/projects.tsx
+++ app/_homePageBlocks/projects.tsx
@@ -43,7 +43,7 @@ export default function Projects(props: { idAttr: string }) {
           <TerminalBlock title={"2025: MyML"}>
             My custom framework for deep learning that helps me understand neural networks, built with Python and numpy. You can check it out on <a href="https://github.com/vidmartin/myml">GitHub</a>.
           </TerminalBlock>
-          <TerminalBlock title={"2024: an interactive 3D application"}>
+          <TerminalBlock title={"2024: Factory rat"}>
             A semestral project for a course in computer graphics, programmed in C++ using OpenGL.
             I also had to create a simple web page for it, which you can marvel at <a href="https://cent.felk.cvut.cz/courses/PGR/archives/2023-2024/S-FIT/vidmamar/">here</a> (it's in Czech).
           </TerminalBlock>
