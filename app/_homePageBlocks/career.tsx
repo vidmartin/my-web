7e1cdf867ab4a8433707587da02500cec1baa5bd
Martin Vidmar <martin.vidmar@seznam.cz>
2025-11-11 21:13:41 +0100

    master: use TerminalBlock's for Career

--- app/_homePageBlocks/career.tsx
+++ app/_homePageBlocks/career.tsx
@@ -1,58 +1,23 @@
 
-type Job = {
-  name: string,
-  role: string,
-  yearFrom: number,
-  yearTo: number,
-  activities: string[],
-};
-
-function JobView(props: { jobs: Job[] }) {
-  const lines: string[] = [];
-
-  for (const job of props.jobs) {
-    lines.push(`+ ${job.name}: ${job.role} (${job.yearFrom} - ${job.yearTo})`);
-    lines.push("|");
-    for (const activity of job.activities) {
-      lines.push(`|   - ${activity}`);
-    }
-    lines.push("|");
-  }
-
-  return <div className="pl-5 pt-5 whitespace-pre-wrap">{lines.join("\n")}</div>
-}
-
-const JOBS: Job[] = [
-  {
-    name: "Marketup",
-    role: "backend developer",
-    yearFrom: 2021,
-    yearTo: 2025,
-    activities: [
-      "development and maintainment of company's internal web application",
-      "rewriting application from jQuery to React",
-      "development of a new GraphQL API to replace an old REST API",
-      "redesigning and extending company's MySQL database",
-      "development of company's Google Cloud infrastructure"
-    ]
-  },
-  {
-    name: "AKSYSTEM",
-    role: ".NET developer",
-    yearFrom: 2020,
-    yearTo: 2022,
-    activities: [
-      "developing add-ons for the POHODA accounting system in .NET",
-      "leveraging POHODA's XML API",
-      "integration with POHODA's MS SQL database",
-      "interacting with various REST APIs",
-    ]
-  }
-];
+import TerminalBlock from "../_components/terminalBlock";
 
 export default function Career(props: { idAttr: string }) {
   return <div className="block" id={props.idAttr}>
     <h1>career</h1>
-    <JobView jobs={JOBS} />
+    <div className="pl-5 pt-5">
+      <TerminalBlock title={"Marketup (2021 - 2025)"}>
+        - development and maintainment of company's internal web application <br />
+        - rewriting application from jQuery to React <br />
+        - development of a new GraphQL API to replace an old REST API <br />
+        - redesigning and extending company's MySQL database <br />
+        - development of company's Google Cloud infrastructure <br />&nbsp;
+      </TerminalBlock>
+      <TerminalBlock title={"AKSYSTEM (2020 - 2022)"}>
+        - developing add-ons for the POHODA accounting system in .NET <br />
+        - leveraging POHODA's XML API <br />
+        - integration with POHODA's MS SQL database <br />
+        - interacting with various REST APIs <br /> &nbsp;
+      </TerminalBlock>
+    </div>
   </div>
 }
--- app/_homePageBlocks/projects.tsx
+++ app/_homePageBlocks/projects.tsx
@@ -1,39 +1,5 @@
+
 import TerminalBlock from "../_components/terminalBlock";
-
-type Project = {
-  name: string,
-  year: number,
-  technologies: string[],
-  description: string,
-};
-
-function ProjectView(props: { projects: Project[] }) {
-  const lines: string[] = [];
-
-  for (const project of props.projects) {
-    lines.push(`+ ${project.year}: ${project.name} (${project.technologies.join(', ')})`);
-    lines.push("|");
-    lines.push(`|   ${project.description}`);
-    lines.push("|");
-  }
-
-  return <div className="pl-5 pt-5 whitespace-pre-wrap">{lines.join("\n")}</div>
-}
-
-const PROJECTS: Project[] = [
-    {
-        name: "MyML",
-        technologies: ["Python", "numpy"],
-        year: 2025,
-        description: "My custom framework for deep learning that helps me understand neural networks."
-    },
-    {
-        name: "an interactive 3D application",
-        technologies: ["C++", "OpenGL"],
-        year: 2024,
-        description: "A semestral project for a course in computer graphics, programmed in C++ using OpenGL. "
-    }
-]
 
 export default function Projects(props: { idAttr: string }) {
     return <div className="block" id={props.idAttr}>
