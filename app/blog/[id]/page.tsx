0d7d7a0a3a3e2b4808c5ca1a9f01a357161ce173
Martin Vidmar <martin.vidmar@seznam.cz>
2025-12-23 22:26:14 +0100

    master: WIP

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,4 +1,8 @@
+
 import { BLOG_POST_REPOSITORY } from "@/app/globals";
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { frontmatter } from "micromark-extension-frontmatter";
+import { frontmatterFromMarkdown } from "mdast-util-frontmatter";
 
 export default async function BlogPost(props: PageProps<"/blog/[id]">) {
     // TODO: get blog post from BLOG_POSTS and render it
@@ -8,11 +12,15 @@ export default async function BlogPost(props: PageProps<"/blog/[id]">) {
 
     const metadata = await BLOG_POST_REPOSITORY.getMetadataById(id);
     const content = await BLOG_POST_REPOSITORY.getContentById(id)
+    const ast = fromMarkdown(content, {
+        extensions: [frontmatter(["yaml"])],
+        mdastExtensions: [frontmatterFromMarkdown(["yaml"])],
+    });
 
     return <>
         <h1 className="text-center mt-10">{metadata.title}</h1>
         <p className="text-center">{metadata.description}</p>
         <p className="text-right mt-5 mb-5"><i>Posted {metadata.publishDate.getFullYear()}-{metadata.publishDate.getMonth()}-{metadata.publishDate.getDate()}</i></p>
-        <div>{content}</div>
+        <div>{JSON.stringify(ast)}</div>
     </>
 }
