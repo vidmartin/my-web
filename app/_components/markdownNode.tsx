1677506eae41a668f507b8314af5477ac651598a
Martin Vidmar <martin.vidmar@seznam.cz>
2025-12-26 20:46:24 +0100

    master: support more markdown constructs

---
+++ app/_components/markdownNode.tsx
@@ -0,0 +1,48 @@
+
+import { Nodes } from "mdast";
+
+export default function MarkdownNode({node}: { node: Nodes }) {
+    if ("children" in node) {
+        const childrenNodes = node.children.map((child, i) => <MarkdownNode key={i} node={child} />);
+        if (node.type == "heading") {
+            if (node.depth == 1) {
+                return <h1 className="mt-10">{childrenNodes}</h1>
+            } else if (node.depth == 2) {
+                return <h2 className="mt-10">{childrenNodes}</h2>
+            } else if (node.depth == 3) {
+                return <h3 className="mt-10">{childrenNodes}</h3>
+            } else {
+                throw new Error(`unsupported heading depth ${node.depth}`);
+            }
+        } else if (node.type == "paragraph") {
+            return <div className="mb-5 text-justify">{childrenNodes}</div>;
+        } else if (node.type == "strong") {
+            return <b>{childrenNodes}</b>;
+        } else if (node.type == "emphasis") {
+            return <i>{childrenNodes}</i>;
+        } else if (node.type == "link") {
+            return <a href={node.url}>{childrenNodes}</a>;
+        } else if (node.type == "root") {
+            return <>{childrenNodes}</>;
+        } else {
+            return <>{JSON.stringify(node)}</>; // unsupported node type
+        }
+    } else {
+        if (node.type == "text") {
+            return <>{node.value}</>;
+        } else if (node.type == "inlineCode") {
+            return <span className="code">{node.value}</span>
+        } else if (node.type == "code") {
+            return <pre className="code mb-5">{node.value}</pre>
+        } else if (node.type == "image") {
+            return <div className="text-center"><img
+                src={node.url}
+                alt={node.alt ?? undefined}
+                title={node.title ?? undefined}
+                className="inline"
+            /></div>
+        } else {
+            return <>{JSON.stringify(node)}</>; // unsupported node type
+        }
+    }
+}
--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -4,35 +4,7 @@ import { fromMarkdown } from "mdast-util-from-markdown";
 import { frontmatter } from "micromark-extension-frontmatter";
 import { frontmatterFromMarkdown } from "mdast-util-frontmatter";
 import { Nodes } from "mdast";
-
-function MarkdownNode({node}: {node: Nodes}) {
-    if (node.type == "heading") {
-        const childrenNodes = node.children.map((child, i) => <MarkdownNode key={i} node={child} />);
-        if (node.depth == 1) {
-            return <h1 className="mt-10">{childrenNodes}</h1>
-        } else if (node.depth == 2) {
-            return <h2 className="mt-10">{childrenNodes}</h2>
-        } else if (node.depth == 3) {
-            return <h3 className="mt-10">{childrenNodes}</h3>
-        } else {
-            throw new Error(`unsupported heading depth ${node.depth}`);
-        }
-    } else if (node.type == "text") {
-        return <>{node.value}</>;
-    } else if (node.type == "paragraph") {
-        const childrenNodes = node.children.map((child, i) => <MarkdownNode key={i} node={child} />);
-        return <p className="mb-5 text-justify">{childrenNodes}</p>;
-    } else if (node.type == "strong") {
-        const childrenNodes = node.children.map((child, i) => <MarkdownNode key={i} node={child} />);
-        return <b>{childrenNodes}</b>;
-    } else if (node.type == "emphasis") {
-        const childrenNodes = node.children.map((child, i) => <MarkdownNode key={i} node={child} />);
-        return <i>{childrenNodes}</i>;
-    } else if (node.type == "root") {
-        const childrenNodes = node.children.map((child, i) => <MarkdownNode key={i} node={child} />);
-        return <>{childrenNodes}</>;
-    }
-}
+import MarkdownNode from "@/app/_components/markdownNode";
 
 export default async function BlogPost(props: PageProps<"/blog/[id]">) {
     const params = await props.params;
@@ -41,8 +13,8 @@ export default async function BlogPost(props: PageProps<"/blog/[id]">) {
     const metadata = await BLOG_POST_REPOSITORY.getMetadataById(id);
     const content = await BLOG_POST_REPOSITORY.getContentById(id)
     const ast = fromMarkdown(content, {
-        extensions: [frontmatter(["yaml"])],
-        mdastExtensions: [frontmatterFromMarkdown(["yaml"])],
+        // extensions: [frontmatter(["yaml"])],
+        // mdastExtensions: [frontmatterFromMarkdown(["yaml"])],
     });
 
     return <>
