2ee8b555d33ed6535c567ec4e357ba207a9bb31e
Martin Vidmar <martin.vidmar@seznam.cz>
2026-02-07 20:34:56 +0100

    master: custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+
+export default function NotFound() {
+    return <div className="h-[100vh] text-center">
+        <div className="text-9xl mt-25">404</div>
+        <div className="text-4xl">not found</div>
+    </div>;
+}
